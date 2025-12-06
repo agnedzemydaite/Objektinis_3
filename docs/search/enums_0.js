28ced8b82be49cc8f0c57d23ac542015dc839339
agnedzemydaite <agne.dzemydaite@mif.stud.vu.lt>
2025-12-06 14:51:53 +0200

    Add files via upload

---
+++ docs/search/enums_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['isvesties_5frezimas_0',['isvesties_rezimas',['../studentai_8h.html#ad683e9937ead1c315daf07391cc89695',1,'studentai.h']]],
+  ['ivestis_5frezimas_1',['ivestis_rezimas',['../studentai_8h.html#aed51dd7c08bee4da006165bad9d72880',1,'studentai.h']]]
+];
---
+++ docs/search/functions_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['failas_0',['failas',['../classstudentas.html#a329638a8d53aaeb0d1d87d41af86122c',1,'studentas']]],
+  ['failu_5fgener_1',['failu_gener',['../generavimas_8cpp.html#a75cdac8ef2bdc234d9667ae949c37b2a',1,'failu_gener(int k):&#160;generavimas.cpp'],['../generavimas_8h.html#a75cdac8ef2bdc234d9667ae949c37b2a',1,'failu_gener(int k):&#160;generavimas.cpp']]]
+];
